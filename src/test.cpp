aa01d17012ec9a20c8aebd97d854b087d73b0811
Serguei Khasimkhanov <serkhas@hotmail.com>
2019-11-22 02:44:39 -0500

    Got glfw_d and gl3w working, by adding compilation and linking of gl3w

--- src/test.cpp
+++ src/test.cpp
@@ -4,8 +4,37 @@
 
 using namespace std;
 
-int main() {
-	cout << "Hello world! (Press enter)" << endl;
-	getchar();
-}
+//int main() {
+//	cout << "Hello world! (Press enter)" << endl;
+//	getchar();
+//}
 
+int main() {
+	if (!glfwInit()) {
+		printf("failed to initialize GLFW.\n");
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	auto window = glfwCreateWindow(1000, 600, "awesome", nullptr, nullptr);
+	if (!window) {
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	if (gl3wInit()) {
+		printf("failed to initialize OpenGL\n");
+		return -1;
+	}
+
+	printf("OpenGL %s, GLSL %s\n", glGetString(GL_VERSION), glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+	Sleep(30000);
+
+	// ...
+
+	return 0;
+}
