2ff29e9147af53d0d1471d40402d4fb491665143
Serguei Khasimkhanov <serkhas@hotmail.com>
2019-11-22 04:04:05 -0500

    create window\!

--- src/test.cpp
+++ src/test.cpp
@@ -2,39 +2,129 @@
 #include "GL/gl3w.h"
 #include "GLFW/glfw3.h"
 
+#include <string>
+#include <fstream>
+#include <streambuf>
+#include <list> 
+#include <vector> 
+#include <iterator> 
+#include <tuple> 
+#include <vmath.h> // TODO: Upgrade version, or use better library?
+#include <math.h>
+
 using namespace std;
 
-//int main() {
-//	cout << "Hello world! (Press enter)" << endl;
-//	getchar();
-//}
+void startup();
+void render(double);
+void shutdown();
+int main();
+
+// TODO: Make everything more object-oriented.
+// That way, I can define functions without having to declare them first, and shit.
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	return main();
+}
 
 int main() {
+	static GLFWwindow *window;
+	static const struct appInfo {
+		const string title = "OpenGL";
+		const bool debug = GL_TRUE;
+		const bool msaa = GL_FALSE;
+		const int width = 800;
+		const int height = 600;
+	} info;
+
+
 	if (!glfwInit()) {
 		printf("failed to initialize GLFW.\n");
 		return -1;
 	}
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// OpenGL 4.5
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+
+	// using OpenGL core profile
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	auto window = glfwCreateWindow(1000, 600, "awesome", nullptr, nullptr);
-	if (!window) {
+	// remove deprecated functionality (might as well, 'cause I'm using gl3w)
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	// disable MSAA
+	glfwWindowHint(GLFW_SAMPLES, info.msaa);
+
+	// debug mode
+	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, info.debug);
+
+	// create window
+	window = glfwCreateWindow(info.width, info.height, info.title.c_str(), nullptr, nullptr);
+	if (!window)
+	{
+		fprintf(stderr, "Failed to open window\n");
 		return -1;
 	}
 
+	// set this window as current window
 	glfwMakeContextCurrent(window);
+
+	//// TODO: set callbacks
+	//glfwSetWindowSizeCallback(window, glfw_onResize);
+	//glfwSetKeyCallback(window, glfw_onKey);
+	//glfwSetMouseButtonCallback(window, glfw_onMouseButton);
+	//glfwSetCursorPosCallback(window, glfw_onMouseMove);
+	//glfwSetScrollCallback(window, glfw_onMouseWheel);
+
+
+	// finally init gl3w
 	if (gl3wInit()) {
 		printf("failed to initialize OpenGL\n");
 		return -1;
 	}
 
-	printf("OpenGL %s, GLSL %s\n", glGetString(GL_VERSION), glGetString(GL_SHADING_LANGUAGE_VERSION));
+	//// TODO: set debug message callback
+	//if (info.flags.debug) {
+	//	if (gl3wIsSupported(4, 3))
+	//	{
+	//		glDebugMessageCallback((GLDEBUGPROC)debug_callback, this);
+	//		glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+	//	}
+	//}
 
-	Sleep(30000);
+	startup();
+
+	// run until user presses ESC or tries to close window
+	while ((glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_RELEASE) && (!glfwWindowShouldClose(window))) {
+		// run rendering function
+		render(glfwGetTime());
+
+		// display drawing buffer on screen
+		glfwSwapBuffers(window);
+
+		// poll window system for events
+		glfwPollEvents();
+	}
+
+	shutdown();
+
 
 	// ...
 
 	return 0;
 }
+
+void startup() {
+
+}
+
+void render(double time) {
+
+}
+
+void shutdown() {
+
+}
+
+
