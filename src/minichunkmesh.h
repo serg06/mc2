7abd4181f35f37aa5ef42ec792efe5845f0fd884
Serguei Khasimkhanov <serkhas@hotmail.com>
2019-12-06 17:06:08 -0500

    remove comments

--- src/minichunkmesh.h
+++ src/minichunkmesh.h
@@ -8,27 +8,6 @@
 #include <vector>
 #include <vmath.h>
 
-//struct Quad {
-//	// block face
-//	uint8_t face;
-//	Block block;
-//
-//	// x and z are 1/16th of reality
-//	uint8_t mini_coords[4];
-//};
-
-//struct Quad {
-//	// for debugging
-//	int size;
-//
-//	// block face
-//	bool is_back_face;
-//	Block block;
-//
-//	// coordinates of corners of quad
-//	ivec3 corners[4];
-//};
-
 // A mesh of a minichunk, consisting of a bunch of quads & minichunk coordinates
 class MiniChunkMesh {
 public:
