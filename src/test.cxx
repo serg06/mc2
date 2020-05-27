21a761b01febea99bc474b32d1e51be2fd5b29db
Serguei <serkhas@hotmail.com>
2020-05-26 23:15:53 -0400

    implement pub-sub proxy with multiple publishers and multiple subscribers

--- src/test.cxx
+++ src/test.cxx
@@ -6,107 +6,122 @@
 
 #define REMOVE
 
-void PublisherThread(zmq::context_t* ctx)
+void BusListener(zmq::context_t* ctx, int idx)
 {
-	//  Prepare publisher
-	zmq::socket_t publisher(*ctx, zmq::socket_type::pub);
-	publisher.bind("inproc://#1");
+	// Connect to bus
+	zmq::socket_t sock(*ctx, zmq::socket_type::sub);
+	sock.setsockopt(ZMQ_SUBSCRIBE, "", 0);
+	sock.connect("inproc://bus-out");
 
-	// Give the subscribers a chance to connect, so they don't lose any messages
-	std::this_thread::sleep_for(std::chrono::milliseconds(20));
-
-	while (true) {
-		//  Write three messages, each with an envelope and content
-		publisher.send(zmq::str_buffer("A"), zmq::send_flags::sndmore);
-		publisher.send(zmq::str_buffer("Message in A envelope"));
-		publisher.send(zmq::str_buffer("B"), zmq::send_flags::sndmore);
-		publisher.send(zmq::str_buffer("Message in B envelope"));
-		publisher.send(zmq::str_buffer("C"), zmq::send_flags::sndmore);
-		publisher.send(zmq::str_buffer("Message in C envelope"));
-		std::this_thread::sleep_for(std::chrono::milliseconds(100));
-	}
-}
-
-void SubscriberThread1(zmq::context_t* ctx)
-{
-	//  Prepare subscriber
-	zmq::socket_t subscriber(*ctx, zmq::socket_type::sub);
-	subscriber.connect("inproc://#1");
-
-	//  Thread2 opens "A" and "B" envelopes
-	subscriber.setsockopt(ZMQ_SUBSCRIBE, "A", 1);
-	subscriber.setsockopt(ZMQ_SUBSCRIBE, "B", 1);
-
-	while (1) {
-		// Receive all parts of the message
+	// Receive messages
+	while (true)
+	{
 		std::vector<zmq::message_t> recv_msgs;
-		zmq::recv_result_t result = zmq::recv_multipart(subscriber, std::back_inserter(recv_msgs));
-		assert(result && "recv failed");
+		const auto ret = zmq::recv_multipart(sock, std::back_inserter(recv_msgs));
+		if (!ret)
+		{
+			OutputDebugString("Listener failed!\n");
+			return;
+		}
 
 		std::stringstream out;
-		out << "Thread2: [" << recv_msgs[0].to_string_view() << "] " << recv_msgs[1].to_string_view() << std::endl;
-		std::cout << out.str();
-#ifdef REMOVE
+		out << "listener [" << idx << "] recv [" << recv_msgs.size() << "] msgs: [";
+		for (int i = 0; i < recv_msgs.size(); i++)
+		{
+			out << recv_msgs[i].to_string_view();
+			if (i != recv_msgs.size() - 1)
+			{
+				out << ", ";
+			}
+		}
+		out << "]\n";
 		OutputDebugString(out.str().c_str());
-#endif // REMOVE
 	}
 }
 
-void SubscriberThread2(zmq::context_t* ctx)
+void BusSender(zmq::context_t* ctx, int idx)
 {
-	//  Prepare our context and subscriber
-	zmq::socket_t subscriber(*ctx, zmq::socket_type::sub);
-	subscriber.connect("inproc://#1");
+	// Connect to bus
+	zmq::socket_t sock(*ctx, zmq::socket_type::pub);
+	sock.connect("inproc://bus-in");
 
-	//  Thread3 opens ALL envelopes
+	// Send messages
+	while (true)
+	{
+		std::array<zmq::const_buffer, 2> send_msgs = {
+			zmq::str_buffer("foo"),
+			zmq::str_buffer("bar!")
+		};
+		const auto ret = zmq::send_multipart(sock, send_msgs);
+		if (!ret)
+		{
+			OutputDebugString("Sender failed!\n");
+			return;
+		}
+
+		std::this_thread::sleep_for(std::chrono::microseconds(1));
+	}
+}
+
+void BusProxy(zmq::context_t* ctx)
+{
+	// create sub/pub
+	zmq::socket_t subscriber(*ctx, zmq::socket_type::sub);
 	subscriber.setsockopt(ZMQ_SUBSCRIBE, "", 0);
+	subscriber.bind("inproc://bus-in");
 
-	while (1) {
-		// Receive all parts of the message
-		std::vector<zmq::message_t> recv_msgs;
-		zmq::recv_result_t result = zmq::recv_multipart(subscriber, std::back_inserter(recv_msgs));
-		assert(result && "recv failed");
+	zmq::socket_t publisher(*ctx, zmq::socket_type::pub);
+	publisher.bind("inproc://bus-out");
 
-		std::stringstream out;
-		out << "Thread3: [" << recv_msgs[0].to_string_view() << "] " << recv_msgs[1].to_string_view() << std::endl;
-		std::cout << out.str();
-#ifdef REMOVE
-		OutputDebugString(out.str().c_str());
-#endif // REMOVE
-	}
+	// connect to creator and tell them we're ready
+	zmq::socket_t pair(*ctx, zmq::socket_type::pair);
+	pair.connect("inproc://bus-status");
+	pair.send(zmq::str_buffer("done"));
+
+	// disconnect
+	pair.close();
+
+	// receive and send repeatedly
+	// TODO: Change to proxy_steerable so we can remotely shut it down
+	zmq::proxy(subscriber, publisher);
+
+	// wew
+	subscriber.close();
+	publisher.close();
 }
 
 int main()
 {
-	/*
-	 * No I/O threads are involved in passing messages using the inproc transport.
-	 * Therefore, if you are using a ØMQ context for in-process messaging only you
-	 * can initialise the context with zero I/O threads.
-	 *
-	 * Source: http://api.zeromq.org/4-3:zmq-inproc
-	 */
+	// Create context
 	zmq::context_t ctx(0);
 
-	auto thread1 = std::async(std::launch::async, PublisherThread, &ctx);
+	// Listen to bus start
+	zmq::socket_t pair(ctx, zmq::socket_type::pair);
+	pair.bind("inproc://bus-status");
 
-	// Give the publisher a chance to bind, since inproc requires it
-	std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	// Start bus
+	auto thread1 = std::async(std::launch::async, BusProxy, &ctx);
 
-	auto thread2 = std::async(std::launch::async, SubscriberThread1, &ctx);
-	auto thread3 = std::async(std::launch::async, SubscriberThread2, &ctx);
-	thread1.wait();
-	thread2.wait();
-	thread3.wait();
+	// Wait for it to start
+	zmq::message_t msg;
+	pair.recv(msg);
+	assert(msg.to_string_view() == "done");
 
-	/*
-	 * Output:
-	 *   An infinite loop of a mix of:
-	 *     Thread2: [A] Message in A envelope
-	 *     Thread2: [B] Message in B envelope
-	 *     Thread3: [A] Message in A envelope
-	 *     Thread3: [B] Message in B envelope
-	 *     Thread3: [C] Message in C envelope
-	 */
+	// Wew!
+	OutputDebugString("Successfully launched bus!\n");
+
+	// Create senders and listeners
+	std::vector<std::future<void>> listeners;
+	std::vector<std::future<void>> senders;
+	for (int i = 0; i < 1; i++)
+	{
+		listeners.push_back(std::async(std::launch::async, BusListener, &ctx, i));
+		senders.push_back(std::async(std::launch::async, BusSender, &ctx, i));
+	}
+
+	OutputDebugString("Launched listeners.\n");
+
+	return 0;
 }
 
 #ifdef REMOVE
